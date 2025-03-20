c0d731cac273a1556005c30660fc4c751273d4aa
sec <sec@192.168.137.1>
2025-03-21 00:36:28 +0900

    branch test

--- TeamMission/src/mission/Manager.java
+++ TeamMission/src/mission/Manager.java
@@ -37,26 +37,25 @@ public class Manager {
 	 * @return
 	 */
 	public static LinkedList<Student> getTxtToList(LinkedList<Student> list) {
-		try {
-			
-			File file = new File("student.txt");
-			Scanner sc = new Scanner(file);
+		File file = new File("student.txt");
+		try (Scanner sc = new Scanner(file)) {
+
 			Student std;
 			while (sc.hasNextLine()) {
 				String stn = sc.nextLine();
 				String[] data = stn.split("/");
-				
-				int no = Integer.parseInt( data[0] );
+
+				int no = Integer.parseInt(data[0]);
 				String name = data[1];
-				int score = Integer.parseInt( data[2] );
+				int score = Integer.parseInt(data[2]);
 				String className = data[3];
-				
+
 				std = new Student(no, name, score, className);
 				list.offer(std);
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		} 
+		}
 		return list;
 	}
 	
